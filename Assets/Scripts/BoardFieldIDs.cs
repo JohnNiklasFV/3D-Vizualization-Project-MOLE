012f54b32986e59d483ede836fb9dda86439889b
Melanie Badt <melanieb@student.uia.no>
2026-04-12 22:43:56 +0200

    Inserted IDs to the Grid Nodes

--- Assets/Scripts/BoardFieldIDs.cs
+++ Assets/Scripts/BoardFieldIDs.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class BoardField : MonoBehaviour
 {
-    public int id; 
+    public int id;
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        if (!enabled) return;
+
+        Handles.color = Color.white;
+
+        Vector3 labelPosition = transform.position + Vector3.up * 0.2f;
+        Handles.Label(labelPosition, $"ID: {id}");
+    }
+#endif
 }
