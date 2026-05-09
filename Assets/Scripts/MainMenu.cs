3d37166f4cd8f6c6ab8dd17d2833b66cd64d6eb7
JoakimGratland <joakimsg@uia.no>
2026-05-09 19:03:29 +0200

    Sprites and scene updates
    Added sprites for Play, Rules and quit
    changed the scene structure because in the build you would be stuck in the sample scene
    checked if all buttons worked in build

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-// Loads up the main scene, 2 is the MainScene build index
+// Loads up the main scene, 1 is the MainScene build index
 public class MainMenu : MonoBehaviour
 {
     public void Playgame()
     {
-        SceneManager.LoadSceneAsync(2);
+        SceneManager.LoadSceneAsync(1);
     }
 
       public void QuitGame()
